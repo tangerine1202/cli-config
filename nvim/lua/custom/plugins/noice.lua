233cd0a3ce730deb7539874362cdaf7654fd9bb0
AlanHuang <alan0988469026@gmail.com>
2025-06-10 16:30:49 +0800

    Move noice cmdline popup to the center

--- nvim/lua/custom/plugins/noice.lua
+++ nvim/lua/custom/plugins/noice.lua
@@ -7,8 +7,13 @@ return {
     'MunifTanjim/nui.nvim',
     -- OPTIONAL:
 
-    --   If not available, we use `mini` as the fallback
-    'rcarriga/nvim-notify',
+    -- If not available, we use `mini` as the fallback
+    {
+      'rcarriga/nvim-notify',
+      -- move the notify to the bottom of the screen
+      -- https://github.com/LazyVim/LazyVim/discussions/2481#discussioncomment-8337157
+      opts = { top_down = false },
+    },
   },
   opts = {
     lsp = {
@@ -22,10 +27,32 @@ return {
     -- you can enable a preset for easier configuration
     presets = {
       bottom_search = true, -- use a classic bottom cmdline for search
-      command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = false, -- add a border to hover docs and signature help
+      command_palette = { -- position the cmdline and popupmenu together
+        -- position the cmdline at the center
+        -- ref: https://github.com/LazyVim/LazyVim/discussions/2481#discussioncomment-8345232
+        views = {
+          cmdline_popup = {
+            position = {
+              row = '50%',
+              col = '50%',
+            },
+            size = {
+              min_width = 60,
+              width = 'auto',
+              height = 'auto',
+            },
+          },
+          cmdline_popupmenu = {
+            position = {
+              row = '67%',
+              col = '50%',
+            },
+          },
+        },
+      },
     },
   },
 }
